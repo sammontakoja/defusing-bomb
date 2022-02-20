f599fa407b0a80cbf10ff25175fe26f231f1c082
Ari Aaltonen <ariaaltonen@pop-os.localdomain>
2022-02-20 13:53:38 +0200

    Created maven project with first draft of the bomb tests. Use Kotlin, JUnit5 and jodd-http libs

---
+++ test/src/test/kotlin/Tests.kt
@@ -0,0 +1,51 @@
+package fi.op.lopoc.los.tests
+
+import jodd.http.HttpRequest
+import jodd.http.HttpResponse
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+
+@Disabled
+class Tests {
+
+    @Test
+    fun `Bomb start to tick after phone call to number 042 486 6247`() {
+        val phoneCallResponse = phoneCall("042 486 6247")
+        assertEquals(200, phoneCallResponse.statusCode())
+        assertEquals("ticktock", phoneCallResponse.bodyText())
+    }
+
+    @Test
+    fun `Bomb disarmed when wires are cut in following order green yellow and blue`() {
+    }
+
+    @Test
+    fun `Bomb explode after two seconds`() {
+    }
+
+    @Test
+    fun `Bomb explode when yellow wire is cut first`() {
+    }
+
+    @Test
+    fun `Bomb explode when blue wire is cut first`() {
+    }
+
+    @Test
+    fun `Bomb explode when green wire is cut first and then blue`() {
+    }
+
+    fun phoneCall(number: String): HttpResponse {
+        return HttpRequest()
+            .host(host)
+            .port(port)
+            .method("POST")
+            .path("phonecall/$number")
+            .send()
+    }
+
+    val host = "localhost"
+    val port = 8080
+
+}
